4c60739e8f9612d988e65495f00695f185419b8a
Philippe Jacquier <phil.jacquier@hotmail.fr>
2024-03-18 09:45:52 +0100

    Nav Artists

--- src/pages/artists.jsx
+++ src/pages/artists.jsx
@@ -1,11 +1,14 @@
 import MenusList from '../components/MenusList'
 import useArtists from '../hooks/useArtists'
+import ArtistsList from '../assets/Artists.json'
 import MainHeader from '../components/MainHeader'
+import { Link } from 'react-router-dom'
 import '../sass/main.scss'
 
 function Artists() {
 
     const artist = useArtists()
+    const indexOfArtist = ArtistsList.findIndex(elem => elem.id === artist.id)
 
     return (
 
@@ -19,12 +22,14 @@ function Artists() {
 
                 <div className="sections">
                     
-                    <div className="shape2"><div className="dates">
-                                <div className="day">{(artist.id).charAt((artist.id.length) - 1) === 'A' ? 'Orange Scene' : 'Little Talk\'s Scene'}</div>
-                                <div className="month">{artist.groupDate.month}</div>
-                                <div className="month">{artist.groupDate.day}<sup>th</sup></div>
-                                <div className="concertTime">{artist.groupDate.dayName} {artist.groupDate.hourStart}</div>
-                            </div></div>
+                    <div className="shape2">
+                        <div className="dates">
+                            <div className="day">{(artist.id).charAt((artist.id.length) - 1) === 'A' ? 'Orange Scene' : 'Little Talk\'s Scene'}</div>
+                            <div className="month">{artist.groupDate.month}</div>
+                            <div className="month">{artist.groupDate.day}<sup>th</sup></div>
+                            <div className="concertTime">{artist.groupDate.dayName} {artist.groupDate.hourStart}</div>
+                        </div>
+                    </div>
                     <div className="groups">
                         <div className="banGroup">
                             <div className="titleBanGroup">{artist.name}<span className="groupFrom">({artist.codeCountry})</span></div>
@@ -43,6 +48,17 @@ function Artists() {
                                     <div className="textGroup2" key={`${text}-${index}`}>{text}</div>
                                 )}
                             </div>
+                        </div>
+
+                        <div className="buttonsNav">
+                            {indexOfArtist !== 0 &&
+                            
+                                <Link to={`/artists/${ArtistsList[indexOfArtist - 1].id}`} className="buttonNav">&lt; Previous Group</Link>
+                            }
+
+                            {indexOfArtist !== (ArtistsList.length - 1) &&
+                                <Link to={`/artists/${ArtistsList[indexOfArtist + 1].id}`} className="buttonNav">Next Group &gt;</Link>
+                            }
                         </div>
                     </div>
                 </div>
